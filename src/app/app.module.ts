7d3d54cbd0a59d441d1e50c1f0b60e48fe6e0425
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-07 11:45:19 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,SpeechRecognition,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    TextToSpeech
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  public matches=[];
+
+  Text_app = [];
+  Text_application = [];
+
+  constructor(
+    private textToSpeech: TextToSpeech,public speechRecognition: SpeechRecognition
+  ) {
+    this.Text_app = [
+      "Bonjour",
+      "Je suis l'application",
+      "je suis ici pour vous aider",
+      "Choisissez une catégorie ou un produit!"      
+    ];
+    this.Text_application = [
+      "Bonjour Je suis l'application je suis ici pour vous aider Choisissez une catégorie ou un produit!"
+    ];
+
+
+  }
+  ngOnInit() {
+  for(let txt of this.Text_application)
+  {
+    this.convertTextToSpeech_en(txt);
+  }
+  }
+  
+   
+  convertTextToSpeech_en(text) {
+      this.textToSpeech.speak({
+        text: text,
+        locale: 'fr-FR',
+        rate: 0.75
+    })
+    .then(() => 
+      console.log('Done')
+    )
+    .catch((reason: any) => 
+      console.log(reason)
+    );
+  }
+
+ 
+  checkPermission(){
+    this.speechRecognition.hasPermission().then((permission)=>{
+      if(permission){
+   
+       alert("Already has permission for speech recognition")
+      }
+      else{
+        alert("Not permission yet")
+      }
+   
+    }, (err)=>{
+     alert(JSON.stringify(err))
+    })
+   
+     }
+   
+     requestPermission(){
+       
+       this.speechRecognition.requestPermission().then((data)=>{
+   
+         alert(JSON.stringify(data))
+   
+       }, (err)=>{
+         alert(JSON.stringify(err))
+       })
+   
+     }
+     
+     startListening(){
+      let options = {
+        language: 'fr-FR'
+      }
+   this.speechRecognition.startListening(options).subscribe((speeches)=>{
+     this.matches=speeches
+   },(err)=>{
+     alert(JSON.stringify(err))
+   })
+   
+     }
+   
+     stopListening(){
+       this.speechRecognition.stopListening()
+   
+     }
+
+}
