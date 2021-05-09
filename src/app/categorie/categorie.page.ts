a143411a64576a5e4712054b51a750daa34c3a7a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-09 10:44:01 +0100

    Update categorie.page.ts

--- src/app/categorie/categorie.page.ts
+++ src/app/categorie/categorie.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Categorie } from '../classes/categorie';
+import { CategorieService } from '../services/categorie.service';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
 
 @Component({
   selector: 'app-categorie',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorie.page.scss'],
 })
 export class CategoriePage implements OnInit {
-
-  constructor() { }
+  categorie:Categorie;
+  categories:Categorie[];
+  constructor(private cateogrieService:CategorieService) { }
+  public matches=[];
 
   ngOnInit() {
+this.categorie=new Categorie();
+      this.read();
   }
+  read()
+{
+  this.cateogrieService.read_Categories().subscribe(data => {
+
+    this.categories = data.map(e => {
+      return {
+       id: e.payload.doc.id,
+
+      
+       titre: e.payload.doc.data()["titre"],
+       description: e.payload.doc.data()["description"],
+
+
+
+      };
+    });
+    console.log(this.categories);
+
+  });
+
+}
+
 
 }
