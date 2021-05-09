c3719e3a7ac38ca7d4adf7d1089a53fcacbc9fa8
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-09 10:30:55 +0100

    Create user.ts

---
+++ src/app/classes/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  password: string;
+  emailVerified: boolean;
+  phoneNumber: string;
+  adresse: string;
+}
