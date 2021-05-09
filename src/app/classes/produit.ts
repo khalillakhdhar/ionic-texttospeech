8c4dd9bf8ff3c2a187662da89f2cea1225ba5cf9
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-09 10:30:46 +0100

    Create produit.ts

---
+++ src/app/classes/produit.ts
@@ -0,0 +1,13 @@
+export class Produit {
+    id:string;
+    titre:string;
+    prix:number;
+    photo:string;
+    categorie:string;
+    quantite_totale:number;
+    description:string;
+    
+
+
+    
+}
