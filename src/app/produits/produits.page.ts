d29734d6b877dc167e34e7d406330dd6cc824007
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-09 10:59:50 +0100

    Create produits.page.ts

---
+++ src/app/produits/produits.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.page.html',
+  styleUrls: ['./produits.page.scss'],
+})
+export class ProduitsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
