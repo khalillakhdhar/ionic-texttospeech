587e6a9ccbc79173fac35bbb6253a81436dde62f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-09 10:30:05 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
-
+//firebase
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
