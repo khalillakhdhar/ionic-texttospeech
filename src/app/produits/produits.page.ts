407319957488aa164c78613813c3c094e7790e60
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-15 19:02:31 +0100

    Update produits.page.ts

--- src/app/produits/produits.page.ts
+++ src/app/produits/produits.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Categorie } from '../classes/categorie';
+import { Produit } from '../classes/produit';
+import { CategorieService } from '../services/categorie.service';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
@@ -6,10 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produits.page.scss'],
 })
 export class ProduitsPage implements OnInit {
-
-  constructor() { }
+choix:string;
+id:string;
+grade:string;
+categories:Categorie[];
+downloadURL: Observable<string>;
+selectedFile: File = null;
+fb = "product";
+produit:Produit;
+produits:Produit[];
+selected=false;
+  constructor(private cateogrieService:CategorieService,private produitService:ProduitService,) { }
 
   ngOnInit() {
+    
+    this.choix=localStorage.getItem("choix");
   }
-
+  read()
+  {
+  this.produitService.read_Produits().subscribe(data => {
+  
+    this.produits = data.map(e => {
+      return {
+        id: e.payload.doc.id,
+  
+         titre: e.payload.doc.data()["titre"],
+         prix: e.payload.doc.data()["prix"],
+         photo: e.payload.doc.data()["photo"],
+         categorie: e.payload.doc.data()["categorie"],
+         quantite_totale: e.payload.doc.data()["quantite_totale"],
+         description: e.payload.doc.data()["description"],
+        
+        
+  
+      };
+    });
+  
+  
+    console.log("liste",this.produits);
+  
+  });
+  
+  
+  
+  }
 }
