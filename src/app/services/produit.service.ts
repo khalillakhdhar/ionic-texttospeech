b1bb9d8518ced18e270adaccb4cdb9ec88432335
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-15 19:02:36 +0100

    Update produit.service.ts

--- src/app/services/produit.service.ts
+++ src/app/services/produit.service.ts
@@ -1,33 +1,30 @@
-import { Injectable } from "@angular/core";
-import { AngularFirestore } from "@angular/fire/firestore";
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root'
 })
-export class Produitservice {
-  id: string;
-  constructor(private firestore: AngularFirestore) {}
+export class ProduitService {
+
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
 
   create_NewProduit(record) {
-    return this.firestore.collection("Produits").add(record);
+    return this.firestore.collection('Produits').add(record);
   }
 
   read_Produits() {
-    return this.firestore.collection("Produits").snapshotChanges();
+    return this.firestore.collection('Produits').snapshotChanges();
   }
-  read_mesProduits() {
-    return this.firestore
-      .collection("Produits", (ref) =>
-        ref.where("id_vd", "==", localStorage.getItem("user"))
-      )
-      .snapshotChanges();
-  }
+
   update_Produit(recordID, record) {
-    this.firestore.doc("Produits/" + recordID).update(record);
-    console.log("updated");
+    this.firestore.doc('Produits/' + recordID).update(record);
+    console.log('updated');
   }
 
   delete_Produit(record_id) {
-    this.firestore.doc("Produits/" + record_id).delete();
+    this.firestore.doc('Produits/' + record_id).delete();
   }
 }
