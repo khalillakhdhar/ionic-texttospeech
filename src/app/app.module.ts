32ca774f190c843d6d6f4e82ebac285a4322f6c2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-31 06:09:37 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AngularFireDatabaseModule } from "@angular/fire/database";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { environment } from "../environments/environment";
 import { LongPressModule } from 'ionic-long-press';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -30,7 +31,8 @@ import { IonicGestureConfig } from './utils/IonicGestureConfig';
     IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
-    SplashScreen,SpeechRecognition,
+    SplashScreen,SpeechRecognition,    BarcodeScanner,
+
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     {provide: HAMMER_GESTURE_CONFIG, useClass: IonicGestureConfig},
     TextToSpeech
